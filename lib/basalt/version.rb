b91a7c3976210d9e1c4ef0717b4eb6625c2251fe
Corey Powell <mistdragon100@gmail.com>
2015-01-02 00:10:48 -0500

    Bump to 0.9.3
    Yeah, I skipped 0.9.1/2

--- lib/basalt/version.rb
+++ lib/basalt/version.rb
@@ -1,6 +1,6 @@
 module Basalt
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 0, 9, 0, nil
+    MAJOR, MINOR, TEENY, PATCH = 0, 9, 3, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
   end
   VERSION = Version::STRING
