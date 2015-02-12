565ca2e9d78111aa1978ea34f9d91c1ea4ba1d41
Corey Powell <mistdragon100@gmail.com>
2015-02-12 12:13:58 -0500

    Fixed issue with basalt config path

--- lib/basalt/config/sys.rb
+++ lib/basalt/config/sys.rb
@@ -4,7 +4,7 @@ module Basalt #:nodoc:
   module Config #:nodoc:
     class Sys < Base
       def initialize
-        super File.join(Dir.home, ENV['BASALT_SYS_CONFIG'] || '.basaltrc')
+        super ENV['BASALT_SYS_CONFIG'] || File.join(Dir.home, '.basaltrc')
       end
     end
   end
