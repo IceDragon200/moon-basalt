b6dc0c620c2fd6ee25f951c61db767130554e264
Corey Powell <mistdragon100@gmail.com>
2015-04-10 16:00:25 -0500

    Bumped version to 0.12.2

--- lib/basalt/version.rb
+++ lib/basalt/version.rb
@@ -1,6 +1,6 @@
-module Basalt #:nodoc:
+module Basalt
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 0, 12, 1, nil
+    MAJOR, MINOR, TEENY, PATCH = 0, 12, 2, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
   end
   VERSION = Version::STRING
